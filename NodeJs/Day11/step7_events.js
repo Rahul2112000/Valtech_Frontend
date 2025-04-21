7e0ed87ba110b37dd8f7796f68050bdff4b98a63
Rahul saini <rajendersaini1492@gmail.com>
2025-04-21 18:02:55 +0530

    Code

--- NodeJs/Day11/step7_events.js
+++ NodeJs/Day11/step7_events.js
@@ -1,10 +1,21 @@
 const Event = require("node:events").EventEmitter;
 let myevent = new Event();
  
-setTimeout(function () {
-  myevent.emit("valtechEvent");
-}, 2000);
+// setTimeout(function () {
+//   myevent.emit("valtechEvent");
+// }, 2000);
  
 myevent.addListener("valtechEvent", function () {
   console.log("Valtech Event Happened");
 });
+
+//dispatch an event once every 4 seconds and 5 times only
+let count = 0;
+let interval=setInterval(function(){
+  myevent.emit("valtechEvent");
+  
+  count++;
+  if(count==5){
+    clearInterval(interval);
+  }
+},4000)
